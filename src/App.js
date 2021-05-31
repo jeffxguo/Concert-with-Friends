58301595e5d890584cae7741fcbe21afb7ad6a63
ruonan <ruonanjia54@gmail.com>
2021-05-30 19:40:22 -0600

    init react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import EventPage from './components/EventPage';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <EventPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/EventPage.js
@@ -0,0 +1,7 @@
+export default function EventPage() {
+    return (
+        <div>
+            Event Page
+        </div>
+    )
+}
