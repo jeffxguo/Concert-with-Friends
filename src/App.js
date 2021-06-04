b5ab9a45c744678bc554830d744c1b15131057ba
Nancy Wan <q2d1b@ugrad.cs.ubc.ca>
2021-06-03 21:43:04 -0700

    Implemented Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, { useState } from 'react';
 import './App.css';
 import EventPage from './components/EventPage';
+import LoginPage from './components/LoginPage';
 import Navbar from './components/Navbar';
+import { Route, Link, Redirect, Switch, withRouter, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isRedirected, setRedirect] = useState(false);
+
   return (
     <div className="App">
-      <Navbar />
-      <EventPage />
+      <Navbar loggedIn={isLoggedIn} handleClick={() => setRedirect(true)}/>
+      <Router>
+        <Switch>
+              <Route path="/login" render={() => <LoginPage />}/>
+              <Route path="/" render={() => isRedirected ? <Redirect to={{ pathname: '/login' }} /> : <EventPage />}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,69 @@
-export default function Navbar() {
-    return (
-        <div>
-            Navbar
+import { AppBar, Button, Menu, MenuItem, Toolbar, Typography, IconButton, makeStyles } from '@material-ui/core';
+import NavigationRoundedIcon from '@material-ui/icons/NavigationRounded';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+import { useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1
+	},
+	menuButton: {
+		marginRight: theme.spacing(2)
+	},
+	title: {
+		flexGrow: 1
+	},
+    location: {
+        fontSize: 20
+    }
+}));
+
+export default function Navbar(props) {
+	const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    }
+	return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="navigation">
+                        <NavigationRoundedIcon />
+                        <a className={classes.location}>Vancouver</a>
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Concert w/ Friends
+                    </Typography>
+                    <Button color="inherit">Groups</Button>
+                    {
+                        props.loggedIn ? 
+                        <div>
+                        <IconButton color="inherit" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                            <AccountCircleRoundedIcon />
+                        </IconButton>
+                        <Menu
+                          id="simple-menu"
+                          anchorEl={anchorEl}
+                          keepMounted
+                          open={Boolean(anchorEl)}
+                          onClose={handleClose}
+                        >
+                          <MenuItem onClick={handleClose}>Profile</MenuItem>
+                          <MenuItem onClick={handleClose}>My groups</MenuItem>
+                          <MenuItem onClick={handleClose}>Logout</MenuItem>
+                        </Menu>
+                      </div>
+                      : <Button color="inherit" onClick={props.handleClick}>Login</Button>
+                    }
+                    
+                </Toolbar>
+            </AppBar>
         </div>
-    )
+	);
 }
