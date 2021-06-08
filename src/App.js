b8ed3f4f7c4022e298fc3c8aff1fc600d5d5bd85
Jeffrey <jeffxguo@gmail.com>
2021-06-07 22:04:03 -0700

    Implemented simple view of Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import EventPage from './components/EventPage';
 import Navbar from './components/Navbar';
+import EventCard from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <EventPage />
+      <EventCard title="Billie Eillish - Happier Than Ever" address="868 Granville St" price="24.99"/>
     </div>
   );
 }
