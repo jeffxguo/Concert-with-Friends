280188b783ff91aacda66b1cfe2caaa868ba7c0d
Ysabelle <y.lacasandile@alumni.ubc.ca>
2021-06-08 15:18:09 -0700

    updated search bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import EventPage from './components/EventPage';
 import Navbar from './components/Navbar';
+import JSONData from './components/MOCK_DATA.json';
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <EventPage />
-    </div>
-  );
+  const [searchTerm, setSearchTerm] = useState('')
+    return (
+        <div className="SearchBar"> 
+        <input type = "text" 
+        placeholder="Search.."  
+        onChange= {event => {setSearchTerm (event.target.value)}}/>
+        {JSONData.filter((val)=>{
+          if (searchTerm == ""){
+            return val
+          } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                    return val
+          }        }).map((val,key)=>{
+            return <div>
+            {val.first_name}</div>
+        })}
+        </div>
+    );
 }
 
 export default App;
