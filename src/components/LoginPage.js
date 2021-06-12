4828b17ab64401d4defbb86c26660d61d7e8bfc4
Nancy Wan <q2d1b@ugrad.cs.ubc.ca>
2021-06-12 16:20:05 -0700

    Minor styling changes on login component

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Container, Grid, Button, TextField, makeStyles } from '@material-ui/core';
+import { COLORS } from '../constants/Colors';
 
-const useStyles = makeStyles((theme) => ({
-    container: {
-      padding: theme.spacing(3),
-    },
-  }));
+const useStyles = makeStyles({
+    root: {
+	  width: "50%",
+      padding: 50,
+	  marginTop: 150,
+	  border: `0.5px solid ${COLORS.darkBlue}`,
+	  borderRadius: 10,
+	  boxShadow: `1px 1px 3px 3px ${COLORS.darkBlue}`
+    }
+});
 
 export default function LoginPage(props) {
     const classes = useStyles();
@@ -14,7 +20,7 @@ export default function LoginPage(props) {
         props.handleLoginSubmit();
     }
 	return (
-		<Container className={classes.container}>
+		<Container className={classes.root}>
 			<form>
 				<Grid container spacing={3}>
 					<Grid item xs={12}>
@@ -35,8 +41,14 @@ export default function LoginPage(props) {
 						</Grid>
 					</Grid>
 					<Grid item xs={12}>
-						<Button color="secondary" fullWidth type="button" variant="contained" onClick={handleLoginSubmit}>
+						<Button color="primary" fullWidth type="button" variant="contained" onClick={handleLoginSubmit}>
 							Log in
+						</Button>
+					</Grid>
+					<Grid item xs={12} spacing={3}>
+						<span>Don't have an account?</span>
+						<Button color="secondary">
+							Sign in here!
 						</Button>
 					</Grid>
 				</Grid>
--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -2,5 +2,6 @@ export const COLORS = {
     black: "#353535",
     highlight: "#3002E7",
     grey: "#9D9D9D",
-    darkBlue: "#070155"
+    darkBlue: "#070155",
+    lightRed: "#f50057"
 }
