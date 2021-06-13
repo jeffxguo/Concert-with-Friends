366b05320a1eb90bf5982f55ef8dcf10ac37bb21
Nancy Wan <q2d1b@ugrad.cs.ubc.ca>
2021-06-13 12:25:51 -0700

    Wrapped up Signup page

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { IconButton, Grid, Button, TextField, makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import { IconButton, Grid, Button, TextField, MenuItem, makeStyles } from '@material-ui/core';
 import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
 
 const useStyles = makeStyles({
@@ -8,8 +8,32 @@ const useStyles = makeStyles({
     }
 });
 
+const musicTypes = [
+    {
+        label: "Hip-hop",
+        value: "Hip-hop"
+    },
+    {
+        label: "Pop",
+        value: "Pop"
+    },
+    {
+        label: "R&B",
+        value: "R&B"
+    },
+    {
+        label: "Rock'n'roll",
+        value: "Rock'n'roll" 
+    },
+    {
+        label: "None of the above",
+        value: ""
+    }
+]
+
 export default function SignupPage(props) {
     const classes = useStyles();
+    const [musicTaste, setMusicTaste] = useState("");
     const handleSignupSubmit = (e) => {
         e.preventDefault();
         props.handleSignupSubmit();
@@ -22,15 +46,35 @@ export default function SignupPage(props) {
                     </Grid>
 					<Grid item xs={12}>
 						<Grid container spacing={2}>
-                        <Grid item xs={12}>
-								<TextField fullWidth label="Username" name="username" size="small" variant="outlined" />
-						</Grid>
-                        <Grid item xs={12}>
-                            <TextField fullWidth label="Email" name="email" size="small" variant="outlined" />
-                        </Grid>
-							<Grid item xs={12}>
-								<TextField fullWidth label="Password" name="password" size="small" type="password" variant="outlined"/>
-							</Grid>
+                            <Grid item xs={12}>
+                                    <TextField fullWidth label="Username" name="username" size="small" type="text" variant="outlined" required/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField fullWidth label="Email" name="email" size="small" type="email" variant="outlined" required/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                    <TextField fullWidth label="Mobile" name="mobile" size="small" type="tel" variant="outlined" required/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField fullWidth label="Password" name="password" size="small" type="password" variant="outlined" required/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    fullWidth
+                                    select
+                                    label="Select"
+                                    value={musicTaste}
+                                    onChange={(e) => setMusicTaste(e.target.value)}
+                                    helperText="Please select your music taste"
+                                    variant="outlined"
+                                    >
+                                    {musicTypes.map((option, idx) => (
+                                        <MenuItem key={idx} value={option.value}>
+                                        {option.label}
+                                        </MenuItem>
+                                    ))}
+                                    </TextField>
+                            </Grid>
 						</Grid>
 					</Grid>
                     <Grid item xs={12}>
