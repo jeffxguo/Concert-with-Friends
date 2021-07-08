77ed90018a278b497c7ec4850d29b44d5f807b8b
ysa-belle <41035253+ysa-belle@users.noreply.github.com>
2021-07-07 18:51:16 -0700

    updated async functions

--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -7,16 +7,15 @@ const GoogleMaps = ({ latitude, longitude }) => {
   // const [events, setEvents] = useState([])
   const apiKey = "zJPgVpNApZcVc9eYvPnrrjrZkOMgExUO"
   const google = window.google
-  const[currentCity,setCurrentCity]= useState([])
 
   useEffect(() => {
-      // fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=' + apiKey + '&city=vancouver&segmentId=KZFzniwnSyZfZ7v7nJ')
-      //     .then(response => response.json())
-      //     .then(data => setEvents(data._embedded.events));
+    // fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=' + apiKey + '&city=vancouver&segmentId=KZFzniwnSyZfZ7v7nJ')
+    //     .then(response => response.json())
+    //     .then(data => setEvents(data._embedded.events));
   }, []);
 
   // console.log(events)
-  
+
 
   const [currentLoc, setCurrentLoc] = React.useState({
     lat: 49.2780527602363,
@@ -25,31 +24,52 @@ const GoogleMaps = ({ latitude, longitude }) => {
 
 
   const ModelsMap = async (map, maps) => {
-    const response = await fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=' + apiKey + '&city='+currentCity+'&segmentId=KZFzniwnSyZfZ7v7nJ');
+
+    const getCurrentLongLat = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          setCurrentLoc({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        });
+      }
+    }
+
+    const getCurrentCity = async () => {
+      Geocoder.init("AIzaSyDaB9iZHEtafiTwgos1qZF0S6iKuW4UpIo");
+
+      try {
+        const json = await Geocoder.from(currentLoc)
+        let currentCity = json.results[0].address_components[3].long_name
+        return currentCity;
+      } catch (error) {
+        console.warn(error)
+        return null;
+      }
+    }
+
+
+
+    getCurrentLongLat();
+    const currentCity = await getCurrentCity();
+    const response = await fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=' + apiKey + '&city=' + currentCity + '&segmentId=KZFzniwnSyZfZ7v7nJ');
     const data = await response.json();
     const events = data._embedded.events;
-    console.log({events});
-
-    // let dataArray = [];
-    // {
-    //   events.map((markerJson) => dataArray.push(markerJson));
-    // }
-
-    const dataArray = events;
-
-    for (let i = 0; i < dataArray.length; i++) {
+    console.log({ events });
 
 
+    for (let i = 0; i < events.length; i++) {
       const marker = new maps.Marker({
-        position: { lat: parseFloat(dataArray[i]._embedded.venues[0].location.latitude), lng: parseFloat(dataArray[i]._embedded.venues[0].location.longitude) },
+        position: { lat: parseFloat(events[i]._embedded.venues[0].location.latitude), lng: parseFloat(events[i]._embedded.venues[0].location.longitude) },
         map
       })
 
       const infowindow = new maps.InfoWindow({
-        
-        content: "<div style='float:left'><img src='" + dataArray[i].images[0].url + "'></div><div style='float:right; padding: 10px;'><b>" + dataArray[i].name  + "</b><br/>" + dataArray[i]._embedded.venues[0].name + "<br/>" + dataArray[i].dates.start.localDate + "<br/>" + dataArray[i].dates.start.localTime + "<br/>" + '<button onclick="myFunction()">Join</button>'+ "</div>" 
+
+        content: `<div style='float:left'><img src=${events[i].images[0].url}></div><div style='float:right; padding: 10px;'><b>${events[i].name}</b><br/>${events[i]._embedded.venues[0].name}<br/>${events[i].dates.start.localDate}<br/>${events[i].dates.start.localTime}<br/><button onclick="myFunction()">Join</button></div>`
       });
-    
+
       marker.addListener("click", () => {
         infowindow.open({
           anchor: marker,
@@ -58,29 +78,14 @@ const GoogleMaps = ({ latitude, longitude }) => {
         });
       })
     }
-
-
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setCurrentLoc({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-      });
-    }
   }
-  
 
-  Geocoder.init("AIzaSyDaB9iZHEtafiTwgos1qZF0S6iKuW4UpIo");
 
-  Geocoder.from(currentLoc)
-  .then(json => {
-  //var addressComponent = json.results[0].address_components[3].long_name;
-    setCurrentCity(json.results[0].address_components[3].long_name)
-  })
-  .catch(error => console.warn(error));
 
-  console.log(currentCity)
+
+
+
+
 
 
 
@@ -98,4 +103,3 @@ const GoogleMaps = ({ latitude, longitude }) => {
 };
 
 export default GoogleMaps;
-
